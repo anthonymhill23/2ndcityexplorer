0a4f22a609523f35a01d7e5eb6367b4392c0a284
Anthony Hill <anthonymhill23@gmail.com>
2022-04-18 21:58:07 -0400

    added modal and app formatting and index formatting

--- 2ndcityexplorer/src/App.js
+++ 2ndcityexplorer/src/App.js
@@ -8,17 +8,17 @@ import axios from 'axios';
 import Errormodal from './Errormodal';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       cityData: [],
       mapurl: '',
       error: false,
-      errorMessage:'',
+      errorMessage: '',
       showModal: false,
     };
   };
-  hideModal= () => {
+  hideModal = () => {
     this.setState({
       showModal: false,
     })
@@ -29,51 +29,50 @@ class App extends React.Component {
     })
   }
   handleCityCall = async (city) => {
-   try{
-    let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${city}&format=json`);
-  this.setState({
-    cityData: cityData.data
-  });
-  console.log(this.state.cityData);
-  } catch (error) {
-    console.log('error', error.response);
-    this.setState({
-      error: true,
-      showModal: true,
-      errorMessage: `An error has been caught: ${error.response.status} ${error.response.statusText}`
-    });
-    console.log(this.state.errorMessage);
+    try {
+      let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${city}&format=json`);
+      this.setState({
+        cityData: cityData.data[0]      });
+      console.log(this.state.cityData);
+    } catch (error) {
+      console.log('error', error.response);
+      this.setState({
+        error: true,
+        showModal: true,
+        errorMessage: `An error has been caught: ${error.response.status} ${error.response.statusText}`
+      });
+      console.log(this.state.errorMessage);
+    }
   }
-  }
-  render(){
+  render() {
     let cityResults = this.state.cityData.map((city, index) => {
       console.log(index)
-      return(
-      <Results
-      key={index}
-      city={city}
-      />
+      return (
+        <Results
+          key={index}
+          city={city}
+        />
       );
     }
-  );
-  return (
-    <>
-    <Header/>
-    <Cityform
-    handleCityCall={this.handleCityCall}
-    />
-    <main>
-    {cityResults}
-    </main>
-    <Errormodal
-    error={this.state.error}
-    errorMessage={this.state.errorMessage}
-    modalState={this.state.showModal}
-    showModal={this.showModal}
-    hideModal={this.hideModal}
-    />
-        </>
-  );
-}
+    );
+    return (
+      <>
+        <Header />
+        <Cityform
+          handleCityCall={this.handleCityCall}
+        />
+        <main>
+          {cityResults}
+        </main>
+        <Errormodal
+          error={this.state.error}
+          errorMessage={this.state.errorMessage}
+          modalState={this.state.showModal}
+          showModal={this.showModal}
+          hideModal={this.hideModal}
+        />
+      </>
+    );
+  }
 }
 export default App;
